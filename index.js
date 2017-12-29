3456d608fb9411666ef4bc13156dfb44445cfc5f
Stephane <son.trinh.dev@gmail.com>
2017-12-29 02:51:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = "I don't do much."
 var kittens = []
+
 function destructivelyAppendKitten(kitten) {
   kittens.push(kitten);
+  return kittens
+}
+
+function destructivelyPrependKitten(kitten) {
+  kittens.unshift(kitten);
   return kittens
 }
